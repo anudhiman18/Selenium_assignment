0da47d2d949d18396e085cc2c1901b50f48740b8
Anu <anu@cloudeq.com>
2023-12-22 16:33:55 +0530

    change

---
+++ src/App.java
@@ -0,0 +1,81 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.*;
+public class App {
+    public static void main(String[] args) throws Exception {
+     
+        System.setProperty("webdriver.edge.driver", "C:\\Users\\Anu\\Downloads\\edgedriver_win64\\msedgedriver.exe");
+ 
+       
+        WebDriver driver = new EdgeDriver();
+ 
+        String baseUrl = "https://demo.guru99.com/test/newtours/register.php";
+        driver.get(baseUrl);
+       
+        WebElement firstNameInput = driver.findElement(By.name("firstName"));
+        firstNameInput.sendKeys("Anu");
+ 
+        WebElement lastNameInput = driver.findElement(By.name("lastName"));
+        lastNameInput.sendKeys("Dhiman");
+ 
+        WebElement phone = driver.findElement(By.name("phone"));
+        phone.sendKeys("9047676476");
+ 
+        WebElement email = driver.findElement(By.id("userName"));  
+        email.sendKeys("anu@cloudeq.com");
+ 
+        WebElement addressInput = driver.findElement(By.name("address1"));
+        addressInput.sendKeys("godrej eternia");
+ 
+        WebElement cityInput = driver.findElement(By.name("city"));
+        cityInput.sendKeys("Chandigarh");
+ 
+        WebElement stateInput = driver.findElement(By.name("state"));
+        stateInput.sendKeys("Punjab");
+ 
+        WebElement codeInput = driver.findElement(By.name("postalCode"));
+        codeInput.sendKeys("123456");
+ 
+        WebElement drpCountry = driver.findElement(By.name("country"));
+        drpCountry.sendKeys("India");
+ 
+ 
+        WebElement username = driver.findElement(By.name("email"));
+        username.sendKeys("anu@cloudeq.com");
+ 
+        WebElement password = driver.findElement(By.name("password"));
+        password.sendKeys("anu@cloudeq.com");
+ 
+        WebElement confirmPassword = driver.findElement(By.name("confirmPassword"));
+       confirmPassword.sendKeys("anu@cloudeq.com");
+         WebElement Login = driver.findElement(By.name("submit"));
+         Login.click();
+
+       
+       
+       
+       
+ 
+       
+ 
+ 
+ 
+ 
+       
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+        Thread.sleep(10000);
+       
+       
+    }
+}
+ 
+ 
